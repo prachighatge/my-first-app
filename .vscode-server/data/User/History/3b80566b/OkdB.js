8281fcacc624d449e982a5fb0fd11c03d89c9459
Prachi Ghatge <prachi.ghatge@camascope.com>
2025-06-05 07:41:29 +0100

    Remove .vscode-server directory from repo

--- .vscode-server/data/User/History/3b80566b/OkdB.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client'; // 👈 This is different
-import App from './App';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
